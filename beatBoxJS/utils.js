4a6d0be1459659ded6e4994cf280b2deb98ed668
kimi <keddy406@hotmail.com>
2020-07-19 15:55:06 +0800

    first commit

---
+++ beatBoxJS/utils.js
@@ -0,0 +1,59 @@
+/**
+ * Beat class that keeps track of playing the audio
+
+ */
+class Beat {
+    constructor(audioSrc) {
+        this.audio = new Audio(audioSrc)
+    }
+    play = () => {
+        this.audio.currentTime = 0;
+        this.audio.play();
+    }
+}
+
+
+
+/**
+ * Button class that keeps track of the button color based on a press
+ */
+class Button {
+    constructor(color, keyCode) {
+        //get html element that can do somefunctions
+       
+        this.color = color;
+        this.keyCode = keyCode;
+        this.element = document.getElementById(keyCode)
+        this.setButtonColorInHTML();
+        this.setTransitionEnd();
+    }
+
+    /**
+     * Set the button color based on color specified
+     */
+    setButtonColorInHTML = () => {
+        this.element.style.borderColor = this.color;
+    }
+
+    /**
+     * Select function to set the background color and boxShadow
+     */
+    select = () => {
+        this.element.style.backgroundColor = this.color;
+        this.element.style.boxShadow = `0px 0px 17px 0px ${this.color}`;
+    }
+
+    setTransitionEnd = () => {
+        this.element.addEventListener("transitionend",()=>{
+            deselect();
+        })
+
+    }
+    /**
+     * Deselect function to reset background color and boxShadow
+     */
+    deselect = () => {
+        this.element.style.backgroundColor = "transparent";
+        this.element.style.boxShadow = "none";
+    }
+}
